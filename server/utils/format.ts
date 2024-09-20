9e3c39cd384a4126560e9ef3b673d5fa3c1be487
nasimulhasan <aparstechteam@gmail.com>
2024-09-20 14:30:40 +0600

    server time solved

--- server/utils/format.ts
+++ server/utils/format.ts
@@ -1,8 +1,4 @@
-import { format, toZonedTime } from "date-fns-tz";
-
-const tz = "Asia/Dhaka";
-
 export const formatDate = (date: string) => {
-  let time = new Date(toZonedTime(date, tz));
+  let time = new Date(date);
   return time;
 };
