f6a7cbf66b7bfa5602e0bc088769a41bd8b06237
nasimulhasan <aparstechteam@gmail.com>
2024-09-27 13:58:21 +0600

    render with katex

--- pages/[shop]/[slug]/mcq/[id]/index.vue
+++ pages/[shop]/[slug]/mcq/[id]/index.vue
@@ -1,61 +1,64 @@
 <template>
 
- <div class="min-h-screen bg-white ">
-    <header class="sticky top-0 z-10 bg-white border-b border-gray-200 shadow-sm">
-        <AppContainer>
-            <div class="container flex items-center justify-between px-2 py-4 mx-auto md:px-4">
-                <h1 class="hidden text-xl font-bold text-gray-800 md:block">{{ data.exam.title }}</h1>
-                <div class="flex items-center justify-between flex-1 space-x-4 md:justify-end">
-                    <div v-if="end_time" class="flex gap-4 text-lg font-semibold text-slate-800">
-                        <!-- Time Left: -->
-                        <ExamTimer :end_time="end_time" />
+    <div class="min-h-screen bg-white ">
+        <header class="sticky top-0 z-10 bg-white border-b border-gray-200 shadow-sm">
+            <AppContainer>
+                <div class="container flex items-center justify-between px-2 py-4 mx-auto md:px-4">
+                    <h1 class="hidden text-xl font-bold text-gray-800 md:block">{{ data.exam.title }}</h1>
+                    <div class="flex items-center justify-between flex-1 space-x-4 md:justify-end">
+                        <div v-if="end_time" class="flex gap-4 text-lg font-semibold text-slate-800">
+                            <!-- Time Left: -->
+                            <ExamTimer :end_time="end_time" />
+
+                        </div>
+                        <Button @click="submitAns"
+                            class="px-4 py-3 font-semibold text-white transition-all duration-300 transform shadow-lg rounded-xl bg-gradient-to-r from-primary to-blue-600 hover:from-blue-600 hover:to-primary hover:scale-105 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
+                            Submit Exam
+                        </Button>
+                    </div>
+                </div>
+            </AppContainer>
+            <Progress v-model="progress" class="w-full rounded-none " />
+        </header>
+
+
+        <div class="max-w-2xl py-5 mx-auto space-y-4 md:py-10 md:space-y-6" v-if="status === 'success'">
+            <h1 class="text-xl font-bold text-center title_grad md:hidden">{{ data.exam.title }}</h1>
+            <div v-for="(q, i) in data.questions" :key="i"
+                class="p-3 mx-2 space-y-3 bg-white border rounded-lg shadow-md md:p-6">
+
+                <div class="text-lg font-semibold text-gray-900 " v-html="q.question"></div>
+
+                <div class="flex flex-wrap gap-3 ">
+                    <Badge> প্রশ্ন নংঃ {{ formatNumber(i + 1) }}</Badge>
+                    <Badge> {{ q.subject.name }}</Badge>
+                    <Badge> ১ নম্বর</Badge>
+                </div>
+                <div class="mt-3 space-y-3 ">
+
+                    <div v-for="(a, j) in q.options" :key="j"
+                        class="flex items-center p-3 space-x-2 text-gray-900 transition-colors border-2 border-gray-200 rounded-lg cursor-pointer hover:bg-gray-50"
+                        :class="{ 'border-slate-500': data.questions[i].selected == a.id }"
+                        @click="selectOption(i, a.id)">
+                        <ClientOnly>
+                            <div v-katex="a.option_text">
+                            </div>
+                        </ClientOnly>
+
 
                     </div>
-                    <Button @click="submitAns"
-                        class="px-4 py-3 font-semibold text-white transition-all duration-300 transform shadow-lg rounded-xl bg-gradient-to-r from-primary to-blue-600 hover:from-blue-600 hover:to-primary hover:scale-105 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
-                        Submit Exam
-                    </Button>
                 </div>
+                <p class="flex items-center mt-4 text-green-600" v-if="q.selected">
+                    <Icon name="lucide:circle-check" class="w-4 h-4 mr-2" />
+                    Answer selected
+                </p>
             </div>
-        </AppContainer>
-        <Progress v-model="progress" class="w-full rounded-none " />
-    </header>
+        </div>
 
-
-    <div class="max-w-2xl py-5 mx-auto space-y-4 md:py-10 md:space-y-6" v-if="status === 'success'">
-        <h1 class="text-xl font-bold text-center title_grad md:hidden">{{ data.exam.title }}</h1>
-        <div v-for="(q, i) in data.questions" :key="i"
-            class="p-3 mx-2 space-y-3 bg-white border rounded-lg shadow-md md:p-6">
-
-            <div class="text-lg font-semibold text-gray-900 " v-html="q.question"></div>
-
-            <div class="flex flex-wrap gap-3 ">
-                <Badge> প্রশ্ন নংঃ {{ formatNumber(i + 1) }}</Badge>
-                <Badge> {{ q.subject.name }}</Badge>
-                <Badge> ১ নম্বর</Badge>
-            </div>
-            <div class="mt-3 space-y-3 ">
-
-                <div v-for="(a, j) in q.options" :key="j"
-                    class="flex items-center p-3 space-x-2 text-gray-900 transition-colors border-2 border-gray-200 rounded-lg cursor-pointer hover:bg-gray-50"
-                    :class="{ 'border-slate-500': data.questions[i].selected == a.id }" @click="selectOption(i, a.id)">
-
-
-                    <AppMath v-model="a.option_text">
-                    </AppMath>
-                </div>
-            </div>
-            <p class="flex items-center mt-4 text-green-600" v-if="q.selected">
-                <Icon name="lucide:circle-check" class="w-4 h-4 mr-2" />
-                Answer selected
-            </p>
+        <div v-else>
+            <AppLoader />
         </div>
     </div>
-
-    <div v-else>
-        <AppLoader />
-    </div>
- </div>
 
 </template>
 
@@ -105,8 +108,8 @@ const submitAns = async () => {
     if (data.value.exam.instantResult) {
         navigateTo(`${route.params.id}/solution`)
     } else {
-         navigateTo(`/${route.params.shop}/${route.params.slug}/mcq`)
-     }
+        navigateTo(`/${route.params.shop}/${route.params.slug}/mcq`)
+    }
 }
 
 const selectOption = (idx, a_id) => {
